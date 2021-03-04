463d1a9a8961f7599c166de6c9c2e35e57981e98
Aaron Lee <aaronlab.net@gmail.com>
2021-03-05 00:49:28 +0900

    custom user model

--- recipe-app-api/app/core/tests/test_admin.py
+++ recipe-app-api/app/core/tests/test_admin.py
@@ -32,3 +32,10 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEquals(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEquals(res.status_code, 200)
