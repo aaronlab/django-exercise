8adc82b33b27866fc0353308b78496f1baa41eb3
Aaron Lee <aaronlab.net@gmail.com>
2021-03-05 23:55:58 +0900

    add results

--- book/ch3/polls/views.py
+++ book/ch3/polls/views.py
@@ -20,6 +20,7 @@ def detail(request, question_id):
 
 
 def vote(request, question_id):
+    """Vote"""
     question = get_object_or_404(Question, pk=question_id)
 
     try:
@@ -36,3 +37,10 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def results(request, question_id):
+    """Results"""
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
