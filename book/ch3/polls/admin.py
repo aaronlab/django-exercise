31ee3d21c642f1b6599b36acd4174f16be985a8e
Aaron Lee <aaronlab.net@gmail.com>
2021-03-06 22:55:24 +0900

    Admin settings

--- book/ch3/polls/admin.py
+++ book/ch3/polls/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
 
-from polls.models import Question, Choice
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Change order to show
+    # fields = ['pub_date', 'question_text']
+
+    # Separate fields to show
+    # fieldsets = [
+    #     ('Question Statement', {'fields': ['question_text']}),
+    #     ('Date Information', {'fields': ['pub_date']}),
+    # ]
+
+    # Collapse fields to show
+    fieldsets = [
+        ('Question Statement', {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+
+    # To show many FKs at the same time
+    inlines = [ChoiceInline]
+
+    # To show additional info
+    list_display = ('question_text', 'pub_date')
+
+    # To show filter options
+    list_filter = ['pub_date']
+
+    # To add search field
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
