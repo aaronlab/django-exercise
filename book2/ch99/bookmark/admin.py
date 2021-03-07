c8a5e6a66a964f8bd8f1c6363752e02c8d5e4867
Aaron Lee <aaronlab.net@gmail.com>
2021-03-07 19:01:21 +0900

    Add Bookmark Model

--- book2/ch99/bookmark/admin.py
+++ book2/ch99/bookmark/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bookmark
+
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url',)
