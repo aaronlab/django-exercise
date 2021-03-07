e562f7ff2c4e8259903e93e16ecf8ebbaa067ef6
Aaron Lee <aaronlab.net@gmail.com>
2021-03-07 19:16:29 +0900

    add models

--- book2/ch99/bookmark/views.py
+++ book2/ch99/bookmark/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Bookmark
 
-# Create your views here.
+
+class BookmarkLV(ListView):
+    model = Bookmark
+
+
+class BookmarkDV(DetailView):
+    model = Bookmark
--- book2/ch99/mysite/urls.py
+++ book2/ch99/mysite/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bookmark.views import BookmarkLV, BookmarkDV
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('bookmark/', BookmarkLV.as_view(), name='index'),
+    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
 ]
