e52c9809635162b52352a75c25ef4b0a60782237
Aaron Lee <aaronlab.net@gmail.com>
2021-03-13 22:54:04 +0900

    add admin test

---
+++ recap-recipe/app/core/tests/test_admin.py
@@ -0,0 +1,28 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@admin.admin',
+            password='password12345',
+            name='admin'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.test',
+            password='testPassword',
+            name='Test User'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
