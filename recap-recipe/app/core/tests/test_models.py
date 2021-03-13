7cf0204b07a1cb0ad6b62dd23023eb77c4315f76
Aaron Lee <aaronlab.net@gmail.com>
2021-03-13 22:42:19 +0900

    normalize email

--- recap-recipe/app/core/tests/test_models.py
+++ recap-recipe/app/core/tests/test_models.py
@@ -19,3 +19,18 @@ class ModelTests(TestCase):
         self.assertEquals(user.email, email)
         self.assertEquals(user.name, name)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@TEST.TEST'
+        password = 'TestPassword'
+        name = 'Test User'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+            name=name
+        )
+        self.assertEquals(user.email, email.lower())
+        self.assertEquals(user.name, name)
+        self.assertTrue(user.check_password(password))
