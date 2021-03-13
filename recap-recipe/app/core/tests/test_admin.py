dcc9bc43b9c2c1c056c0f1c0a6f48d378931b67c
Aaron Lee <aaronlab.net@gmail.com>
2021-03-13 22:59:53 +0900

    add change user

--- recap-recipe/app/core/tests/test_admin.py
+++ recap-recipe/app/core/tests/test_admin.py
@@ -26,3 +26,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEquals(res.status_code, 200)
