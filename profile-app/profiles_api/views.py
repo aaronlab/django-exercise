7df1c9bbab03c82328c4e7908d61407e462b3b64
Aaron Lee <aaronlab.net@gmail.com>
2021-03-18 15:41:04 +0900

    add post view

--- profile-app/profiles_api/views.py
+++ profile-app/profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of API features"""
@@ -22,3 +26,21 @@ class HelloApiView(APIView):
                 'an_api_view': an_api_view
             }
         )
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello, {name}!'
+            return Response(
+                {
+                    'message': message
+                }
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
