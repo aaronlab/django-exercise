235027337f17c0d39433ed2c3c932238b0563aae
Aaron Lee <aaronlab.net@gmail.com>
2021-03-18 20:19:48 +0900

    user profile serializer + viewset

--- profile-app/profiles_api/views.py
+++ profile-app/profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 from profiles_api import serializers
+from profiles_api  import models
 
 
 class HelloApiView(APIView):
@@ -134,3 +135,9 @@ class HelloViewSet(viewsets.ViewSet):
                 'pk': pk
             }
         )
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
