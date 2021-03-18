94a7f12112e334f0214cd505f0db437aa727e342
Aaron Lee <aaronlab.net@gmail.com>
2021-03-18 16:41:45 +0900

    viewset crud

--- profile-app/profiles_api/views.py
+++ profile-app/profiles_api/views.py
@@ -60,6 +60,7 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Return a hello message"""
@@ -77,5 +78,59 @@ class HelloViewSet(viewsets.ViewSet):
             {
                 'message': 'Hello!',
                 'a_view_set': a_view_set
+            }
+        )
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello, {name}'
+            return Response(
+                {
+                    'message': message
+                }
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response(
+            {
+                'http_method': 'GET',
+                'pk': pk
+            }
+        )
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response(
+            {
+                'http_method': 'PUT',
+                'pk': pk
+            }
+        )
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response(
+            {
+                'http_method': 'PATCH',
+                'pk': pk
+            }
+        )
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response(
+            {
+                'http_method': 'DELETE',
+                'pk': pk
             }
         )
